55236aa18f8564c8673f473587c884c217139dec
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-06-24 20:39:54 +0300

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,61 +1,70 @@
-// import { NavLink } from "react-router-dom";
 import { TweenMax, TimelineMax, Power3, Power4 } from "gsap";
 
 import './Home.css';
 import TextDiagonal from "../TextDiagonal/TextDiagonal";
 import BtnRotation from "../BtnRotation/BtnRotation";
 
-import { useRef, useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import TitleRotation from "../TitleRotation/TitleRotation";
 
 function Home() {
 
-  let screen = useRef(null);
-  let body = useRef(null);
+    let screen = useRef(null);
+    let body = useRef(null);
   
-  useEffect(() => {                      // TODO: route louder
-    let tl = new TimelineMax();
-    tl.to(screen, {
-      duration: 1.2,
-      height: "100%",
-      ease: Power3.easeInOut,
+    useEffect(() => {                      // TODO: route louder
+        let tl = new TimelineMax();
+
+        tl.to(screen, {
+            duration: 1.2,
+            height: "100%",
+            ease: Power3.easeInOut,
+        });
+
+        tl.to(screen, {
+            duration: 1,
+            top: "100%",
+            ease: Power3.easeInOut,
+            delay: 0.3,
+        });
+
+        tl.set(screen, { left: "-100%" });
+        TweenMax.to(body, .3, {css: {
+            opacity: "1",
+            pointerEvents: "auto",
+            ease: Power4.easeInOut
+        }}).delay(2);
+
+        return () => {
+            TweenMax.to(body, 1, {css: {
+            opacity: "0",
+            pointerEvents: 'none'
+            }});
+        }
     });
-    tl.to(screen, {
-      duration: 1,
-      top: "100%",
-      ease: Power3.easeInOut,
-      delay: 0.3,
-    });
-    tl.set(screen, { left: "-100%" });
-    TweenMax.to(body, .3, {css: {
-      opacity: "1",
-      pointerEvents: "auto",
-      ease: Power4.easeInOut
-    }}).delay(2);
-    return () => {
-      TweenMax.to(body, 1, {css: {
-        opacity: "0",
-        pointerEvents: 'none'
-      }});
-  }
-  });
-  return (
-    <div className="home" >
-      <div className="load-container">
-        <div className="load-screen1" ref={(el) => (screen = el)}>
+
+    return (
+        <div className="home" >
+
+            <div className="load-container">
+                <div className="load-screen1" ref={(el) => (screen = el)}>
+                </div>
+            </div>
+
+            <div data-barba="container" >
+                <div ref={(el) => (body = el)} className="load-head" >
+
+                    <TextDiagonal />
+
+                    <BtnRotation />
+                    
+                    <TitleRotation />
+                
+                </div>
+            </div>
+            
         </div>
-      </div>
-      <div data-barba="container" >
-        <div ref={(el) => (body = el)} className="load-head" >
-          {/* <TextDiagonal /> */}
-          <BtnRotation />
-          
-          <TitleRotation />
-        
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
